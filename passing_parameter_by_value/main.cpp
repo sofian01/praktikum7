2aa257664263036bb07a2c36d63e057310a57910
sofian01 <sopianrinaldi22@gmail.com>
2018-11-30 21:41:26 +0700

    passing_parameter_by_value

---
+++ passing_parameter_by_value/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+void nilai(int a){
+   a= 10;
+   }
+
+int main(){
+   int a = 5;
+   cout << "Nilai awal : " << a << endl;
+   nilai(a);
+   cout << "Nilai akhir:" << a << endl;
+}
